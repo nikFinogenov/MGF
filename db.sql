8f5de10e568bc7bc9031c59641fcdc369f55bf23
Nikita Finogenov <nikita.finogenov@gmail.com>
2024-09-15 21:56:36 +0300

    avatar

--- db.sql
+++ db.sql
@@ -6,7 +6,8 @@ CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    avatar VARCHAR(10) NOT NULL DEFAULT('123.png')
 );
 CREATE USER IF NOT EXISTS 'rootuser'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON mgf.* TO 'rootuser'@'localhost';
