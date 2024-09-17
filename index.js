3cb7dd139c9a61921bbb90236be9ccaf61b7e945
myth cat <xdxddd932@gmail.com>
2024-09-17 05:13:10 +0300

    1 ebanat ne mozhet na arena poka 2 ebanat ne viberet

--- index.js
+++ index.js
@@ -10,113 +10,142 @@ const port = 3000;
 // Создаем HTTP-сервер
 const server = http.createServer(app);
 const io = new Server(server);
-
-// Создаем WebSocket-сервер
-// const wss = new WebSocket.Server({ server });
-// io.on('connection', (socket) => {
-//   const users = [];
-//   console.log('A user connected');
-// const data = JSON.parse(message);
-
-// // Добавляем userEmail в список, если его ещё там нет
-// if (data.userEmail && !users.includes(data.userEmail)) {
-//     users.push(data.userEmail);
-// }
-
-// // Отправляем список всех email-ов всем подключённым клиентам
-// wss.clients.forEach(function each(client) {
-//     if (client.readyState === WebSocket.OPEN) {
-//         client.send(JSON.stringify(users));
-//     }
-// });
-
-//   socket.on('gameFind', (msg) => {
-//       console.log('Message: ' + msg);
-//       io.emit('gameFind', msg);  // Отправляем сообщение всем клиентам
-//   });
-
-//   socket.on('disconnect', () => {
-//       console.log('User disconnected');
-//   });
-// });
 const rooms = {}; // Объект для хранения комнат
 
+function joinRoom() {
+    
+}
+
 io.on('connection', (socket) => {
-  console.log('New player connected:', socket.id);
+    console.log('New player connected:', socket.id);
 
-  // Получаем email пользователя
-  socket.on('userEmail', (email) => {
-    socket.email = email; // Сохраняем email в сокете
-  });
-  socket.on('userAvatar', (avatar) => {
-    socket.avatar = avatar; // Сохраняем email в сокете
-  });
-  socket.on('userName', (name) => {
-    socket.name = name; // Сохраняем email в сокете
-  });
+    
 
-  // socket.on('userId', (idd) => {
-  //   socket.idd = idd; // Сохраняем email в сокете
-  // });
-
-  // Обработчик поиска игры
-  socket.on('searchGame', () => {
-    let roomId = null;
-
-    // Ищем комнату с одним игроком
-    for (let id in rooms) {
-      if (rooms[id].players.length === 1) {
-        roomId = id;
-        break;
-      }
-    }
-
-    // Если нет комнаты, создаем новую
-    if (!roomId) {
-      roomId = `room_${socket.id}`;
-      rooms[roomId] = { players: [] };
-    }
-
-    // Присоединяем игрока к комнате
-    rooms[roomId].players.push({
-      id: socket.id,
-      email: socket.email,
-      name: socket.name,
-      avatar: socket.avatar
+    // Получаем email пользователя
+    socket.on('userEmail', (email) => {
+        socket.email = email; // Сохраняем email в сокете
     });
-    socket.join(roomId);
-    console.log(`Player ${socket.id} joined room ${roomId}`);
+    socket.on('userAvatar', (avatar) => {
+        socket.avatar = avatar; // Сохраняем email в сокете
+    });
+    socket.on('userName', (name) => {
+        socket.name = name; // Сохраняем email в сокете
+    });
 
-    // Если два игрока в комнате, начинаем игру
-    if (rooms[roomId].players.length === 2) {
-      const players = rooms[roomId].players;
+    // socket.on('userId', (idd) => {
+    //   socket.idd = idd; // Сохраняем email в сокете
+    // });
 
-      // Отправляем обоим игрокам информацию о противнике
-      io.to(roomId).emit('startGame', {
-        roomId,
-        players
-      });
-    }
-  });
+    // Обработчик поиска игры
+    socket.on('searchGame', () => {
+        let roomId = null;
 
-  // Обработчик выхода игрока
-  socket.on('disconnect', () => {
-    for (let roomId in rooms) {
-      const room = rooms[roomId];
-      const index = room.players.findIndex(p => p.id === socket.id);
-      if (index !== -1) {
-        room.players.splice(index, 1);
-        socket.leave(roomId);
-        console.log(`Player ${socket.id} left room ${roomId}`);
-
-        // Удаляем комнату, если она пустая
-        if (room.players.length === 0) {
-          delete rooms[roomId];
-          console.log(`Room ${roomId} deleted`);
+        // Ищем комнату с одним игроком
+        for (let id in rooms) {
+            if (rooms[id].players.length === 1) {
+                roomId = id;
+                break;
+            }
         }
-      }
-    }
-  });
+
+        // Если нет комнаты, создаем новую
+        if (!roomId) {
+            roomId = `room_${socket.id}`;
+            rooms[roomId] = { players: [] };
+        }
+
+        // Присоединяем игрока к комнате
+        rooms[roomId].players.push({
+            id: socket.id,
+            email: socket.email,
+            name: socket.name,
+            avatar: socket.avatar
+        });
+        socket.join(roomId);
+        console.log(`Player ${socket.id} joined room ${roomId}`);
+
+        // Если два игрока в комнате, начинаем игру
+        if (rooms[roomId].players.length === 2) {
+            const players = rooms[roomId].players;
+
+            // Отправляем обоим игрокам информацию о противнике
+            io.to(roomId).emit('startGame', {
+                roomId,
+                players
+            });
+        }
+    });
+
+    socket.on('inArena', () => {
+        
+        let roomId = null;
+
+        for (let id in rooms) {
+            if (rooms[id].players.length === 1) {
+                roomId = id;
+                break;
+            }
+        }
+
+        if (!roomId) {
+            roomId = `room_${socket.id}`;
+            rooms[roomId] = { players: [], selections: {} };
+        }
+
+        rooms[roomId].players.push({
+            id: socket.id,
+        });
+
+        socket.join(roomId);
+        console.log(`Player ${socket.id} joined room ${roomId}`);
+
+        socket.on('cardSelected', (data) => {
+            console.log(`Player ${data.playerId} selected card: ${data.card}`);
+    
+            if (roomId) {
+                rooms[roomId].selections[socket.id] = data.card;
+    
+                console.log(`Current selections for room ${roomId}:`, rooms[roomId].selections);
+    
+                // Check if both players have selected their cards
+                const allPlayersSelected = rooms[roomId].players.every(p => rooms[roomId].selections[p.id]);
+    
+                if (allPlayersSelected) {
+                    // Notify both players that the selection is complete
+                    io.to(roomId).emit('cardsSelected', {
+                        players: rooms[roomId].players.map(p => ({
+                            id: p.id,
+                            card: rooms[roomId].selections[p.id]
+                        }))
+                    });
+                    console.log(`Both players have selected their cards. Emitting cardsSelected event.`);
+                    
+                    // Clear selections for the next round or game
+                    rooms[roomId].selections = {};
+                }
+            }
+        });
+    });
+
+
+    // // Обработчик выхода игрока
+    socket.on('disconnect', () => {
+        for (let roomId in rooms) {
+            const room = rooms[roomId];
+            const index = room.players.findIndex(p => p.id === socket.id);
+            if (index !== -1) {
+                room.players.splice(index, 1);
+                socket.leave(roomId);
+                console.log(`Player ${socket.id} left room ${roomId}`);
+
+                // Удаляем комнату, если она пустая
+                if (room.players.length === 0) {
+                    delete rooms[roomId];
+                    console.log(`Room ${roomId} deleted`);
+                }
+            }
+        }
+    });
 });
 
 // Подключаем WebSocket-логику //nahui web-socket logiku, mi socket.io chads teper
@@ -128,59 +157,59 @@ app.use(express.json());
 app.use(express.static('assets'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/login.html');
+    res.sendFile(__dirname + '/views/login.html');
 });
 
 app.post('/register', async (req, res) => {
-  try {
-    let user = new User(req.body.name, req.body.email, req.body.password);
-    await user.save();
-    res.status(200).json({ message: 'User created successfully' });
-  } catch (error) {
-    if (error.message.includes("Duplicate")) {
-      res.status(400).json({ error: 'A user with the same email already exists.' });
-    } else {
-      res.status(500).json({ error: 'An error occurred while registering the user.' });
+    try {
+        let user = new User(req.body.name, req.body.email, req.body.password);
+        await user.save();
+        res.status(200).json({ message: 'User created successfully' });
+    } catch (error) {
+        if (error.message.includes("Duplicate")) {
+            res.status(400).json({ error: 'A user with the same email already exists.' });
+        } else {
+            res.status(500).json({ error: 'An error occurred while registering the user.' });
+        }
     }
-  }
 });
 
 app.put('/user/:userid', async function (req, res) {
-  try {
-    let user = new User(req.body.name, req.body.email, null);
-    user.avatar = req.body.avatar;
-    if (req.body.old) user.password = req.body.old;
-    user.id = req.params.userid;
-    if (req.body.old) await user.savePass(req.body.new);
-    else await user.save();
-    res.status(200).json({ message: 'User updated successfully' });
-  } catch (error) {
-    if (error.message.includes("Duplicate")) {
-      res.status(400).json({ error: 'A user with the same email already exists.' });
-    } else if (error.message.includes("Password mismatch")) {
-      res.status(400).json({ error: 'Current password is wrong' });
-    } else {
-      res.status(500).json({ error: 'An error occurred while updating the user.' });
+    try {
+        let user = new User(req.body.name, req.body.email, null);
+        user.avatar = req.body.avatar;
+        if (req.body.old) user.password = req.body.old;
+        user.id = req.params.userid;
+        if (req.body.old) await user.savePass(req.body.new);
+        else await user.save();
+        res.status(200).json({ message: 'User updated successfully' });
+    } catch (error) {
+        if (error.message.includes("Duplicate")) {
+            res.status(400).json({ error: 'A user with the same email already exists.' });
+        } else if (error.message.includes("Password mismatch")) {
+            res.status(400).json({ error: 'Current password is wrong' });
+        } else {
+            res.status(500).json({ error: 'An error occurred while updating the user.' });
+        }
     }
-  }
 });
 
 app.post('/login', async (request, response) => {
-  try {
-    let user = new User('', request.body.email, request.body.password);
-    let row = await user.checkUser();
-    user.name = row.name;
-    user.id = row.id;
-    user.avatar = row.avatar;
-    response.status(200).json({ user: user, message: 'Logged in successfully' });
-  } catch (error) {
-    if (error.message.includes("Does not match")) {
-      response.status(400).json({ error: "Login or password doesn't match" });
-    } else {
-      console.error(error);
-      response.status(500).json({ error: 'An error occurred while logging in the user.' });
+    try {
+        let user = new User('', request.body.email, request.body.password);
+        let row = await user.checkUser();
+        user.name = row.name;
+        user.id = row.id;
+        user.avatar = row.avatar;
+        response.status(200).json({ user: user, message: 'Logged in successfully' });
+    } catch (error) {
+        if (error.message.includes("Does not match")) {
+            response.status(400).json({ error: "Login or password doesn't match" });
+        } else {
+            console.error(error);
+            response.status(500).json({ error: 'An error occurred while logging in the user.' });
+        }
     }
-  }
 });
 
 // const game = new Game(1, 2);
@@ -188,5 +217,5 @@ app.post('/login', async (request, response) => {
 
 // Запускаем сервер
 server.listen(port, () => {
-  console.log(`Сервер запущен на http://localhost:${port}`);
+    console.log(`Сервер запущен на http://localhost:${port}`);
 });
