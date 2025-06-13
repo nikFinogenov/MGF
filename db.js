0d525683b1e716b1cc1100b1eb0a6db16b3c1e71
Nikita Finogenov <nikita.finogenov@gmail.com>
2025-06-13 14:18:31 +0300

    ssl

--- db.js
+++ db.js
@@ -1,13 +1,17 @@
 
 const mysql = require('mysql2');
 const config = require('./config.json');
+const fs = require('fs');
 require('dotenv').config();
 const pool = mysql.createPool({
   host: process.env.HOST || config.host,
   port: process.env.PORT || config.port,
   user: process.env.DB_USER || config.user,
   password: process.env.PASS || config.password,
-  database: process.env.DB_NAME || config.database
+  database: process.env.DB_NAME || config.database,
+  ssl: {
+    ca: fs.readFileSync(process.env.DB_CA || './certs/ca-cert.pem')  // adjust path if needed
+  }
 });
 
 console.log(pool);
