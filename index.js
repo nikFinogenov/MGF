5a6fa29458c2091242618f882ba0f60fe7bbfd14
Nikita Finogenov <nikita.finogenov@gmail.com>
2025-06-13 16:25:48 +0300

    socket tests

--- index.js
+++ index.js
@@ -390,5 +390,5 @@ app.post('/login', async (request, response) => {
 
 
 server.listen(port, () => {
-    console.log(`Сервер запущен на http://0.0.0.0:${port}`);
+    console.log(`Сервер запущен`);
 });
